bef4aa2e76d77da54a603401ce415c8436b3ca77
jenishj123 <37879477+jenishj123@users.noreply.github.com>
2021-05-09 19:34:55 -0400

    Add files via upload

---
+++ StarterCode/app.js
@@ -0,0 +1,41 @@
+function dropdown(){
+    d3.json("samples.json).then(function(data){
+        var dropdownid = data.names
+    })
+
+}
+
+
+
+
+
+function buildtable(id){
+
+     d3.json("samples.json).then(function(data){
+        var metadata= data.metadata
+    })
+
+
+}
+
+
+
+
+
+function buildcharts(id){
+ d3.json("samples.json).then(function(data){
+        var samples = data.samples
+    })
+
+}
+
+
+
+
+
+function optionChanged(id){
+    buildtable(id)
+    buildcharts(id)
+}
+
+dropdown()
